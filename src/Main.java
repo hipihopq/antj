fc7ea674d9f82d58b17d2881a807fa2ea9c463f2
hipihopq <rutkovskayadaria@gmail.com>
2024-12-10 14:25:23 +0300

    Test to trigger Jenkins

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Ant!");
+        System.out.println("Hello, Ant! This is just to trigger you");
     }
 }
